7a0d82d02981af88b461b1e9d0c333d7b4ba9fbb
mred5683 <hansenrd5683@gmail.com>
2016-12-03 22:41:13 -0700

    add binarytriangle to repo

---
+++ BinaryTriangle/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using static System.Console;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTriangle {
+  class Program {
+    static void Main(string[] args) {
+      int p, lastInt = 0, input;
+      Console.WriteLine("Enter the Number of Rows : ");
+      input = int.Parse(Console.ReadLine());
+      for (var i = 1; i <= input; i++) {
+        for (p = 1; p <= i; p++)
+          if (lastInt == 1) {
+            Console.Write("0");
+            lastInt = 0;
+          }
+          else if (lastInt == 0) {
+            Console.Write("1");
+            lastInt = 1;
+          }
+        Console.Write("\n");
+      }
+      Console.ReadLine();
+    }
+  }
+}
