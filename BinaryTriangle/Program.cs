9cac423d49b32e31c2f87d6834c938c65fdff3f4
mred5683 <hansenrd5683@gmail.com>
2016-12-13 07:08:18 -0700

    changed variable names for readability, added temp variables which will be used to build the initial triangle.

--- BinaryTriangle/Program.cs
+++ BinaryTriangle/Program.cs
@@ -9,20 +9,22 @@ namespace BinaryTriangle {
 
   class Program {
 
-    private static void Main(string[] args) {
+    public static void Main() {
 
       WriteLine("Enter a phrase to convert to binary...");
       var userInput = ReadLine();
       var userInputToBinary = new StringBuilder();
-      var addSymbolToBeginningOfUserInputToBinary = new StringBuilder();
-      var temp1 = 0;
-      var temp2 = 1;
 
-      var listOfTriSequence = new List<int>();
-      
+
+
       //convert userInput to Binary.
-      foreach (char ch in userInput) {
-        userInputToBinary.Append(Convert.ToString(ch, 2));
+      if (userInput == null) {
+        WriteLine("must enter valid text...");
+        Main();
+      }
+
+      else {
+        foreach (var ch in userInput) userInputToBinary.Append(Convert.ToString(ch, 2));
       }
 
       WriteLine(userInputToBinary.Length);
@@ -31,25 +33,31 @@ namespace BinaryTriangle {
       //Gets the triangle number sequence. sequence shows how many "characters" total to make a complete triangle.
       //first row has one, second row has 2, third row has three etc....
 
-      listOfTriSequence.Add(temp2);
+      var tempToCreateList1 = 0;
+      var tempToCreateList2 = 1;
+
+      var listOfTriSequence = new List<int> {tempToCreateList2};
+
       while (listOfTriSequence.Max() <= lengthOfUserInputToBinary) {
-        var temp = temp2 - temp1;
-        while (temp1 != temp2) {
-          temp1 += 1;
+        var tempToCreateList = tempToCreateList2 - tempToCreateList1;
+        while (tempToCreateList1 != tempToCreateList2) {
+          tempToCreateList1 += 1;
         }
 
-        while (temp + 1 != (temp2 - temp1)) {
-          temp2 += 1;
+        while (tempToCreateList + 1 != (tempToCreateList2 - tempToCreateList1)) {
+          tempToCreateList2 += 1;
         }
-        listOfTriSequence.Add(temp2);
+
+        listOfTriSequence.Add(tempToCreateList2);
       }
-      
+
       //created a tempUserInputToBinaryLength variable to hold the value of how many characters is needed to fill in
-      //the different between listOfTriSequence.Max() and userInputToBinary.Length (if applicable).  in order to make
-      //a complete triangle the value needs to equal a value in the triangle number sequence. 
+      //the difference between listOfTriSequence.Max() and userInputToBinary.Length (if applicable).  in order to make
+      //a 'complete' triangle, the value needs to equal a value in the triangle number sequence. 
       //example ::  https://en.wikipedia.org/wiki/Triangular_number
 
       var tempUserInputToBinaryLength = userInputToBinary.Length;
+      var addSymbolToBeginningOfUserInputToBinary = new StringBuilder();
 
       while (tempUserInputToBinaryLength < listOfTriSequence.Max()) {
         addSymbolToBeginningOfUserInputToBinary.Append("*");
@@ -60,8 +68,16 @@ namespace BinaryTriangle {
           break;
         }
       }
+
       WriteLine(addSymbolToBeginningOfUserInputToBinary.ToString());
-      var stringSymbolsAndUserInputToBinary = addSymbolToBeginningOfUserInputToBinary.ToString();
+      var appendedSymbolsAndUserInputToBinary = addSymbolToBeginningOfUserInputToBinary.ToString();
+
+      //while (temp < temp1) {
+      //  var temp = 0;
+      //  var temp1 = 0;
+      //  var temp2 = 0;
+      //}
+
       //foreach (char ch in stringSymbolsAndUserInputToBinary) {
       //  WriteLine(stringSymbolsAndUserInputToBinary);
       //}
@@ -69,6 +85,9 @@ namespace BinaryTriangle {
       ReadKey();
     }
 
+
+    ////Code for creating a triangle with alternating 0's and 1's
+    
     //int p, lastInt = 0, input;
     //WriteLine("Enter the Number of Rows : ");
     //input = int.Parse(ReadLine());
@@ -89,5 +108,4 @@ namespace BinaryTriangle {
     //ReadLine();
 
   }
-
 }
